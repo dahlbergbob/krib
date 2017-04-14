943d0106a15cf4308a6d7994118142cd9f42e622
Bob Dahlberg <dahlberg.bob@gmail.com>
2017-04-14 15:18:46 +0200

    Added more declarative way of using the extension functions

--- src/main/kotlin/se/accepted/krib/extensions.kt
+++ src/main/kotlin/se/accepted/krib/extensions.kt
@@ -4,8 +4,15 @@ package se.accepted.krib
  * Extension functions for the library
  * @author Bob Dahlberg
  */
+object App{
+    val krib = Krib()
+    init {
+        // Bootstrap
+        // krib.add()
+    }
+}
 // Use if you have global static access to your krib instance
-// inline fun <reified T:Any> inject() = lazy { krib.require( T::class ) }
+inline fun <reified T:Any> inject() = lazy { App.krib.require( T::class ) }
 
 // Use if you need to inject the proper krib instance to require from
-inline fun <reified T:Any> inject( from:Krib) = lazy { from.require( T::class ) }
+inline fun <reified T:Any> inject( from:Krib ) = lazy { from.require( T::class ) }
